65bb7562ce15bf3c123104927aa24cfe0b79d173
Alex <alex85456@gmail.com>
2024-11-03 15:12:49 +0100

    updated dependencies for embedded-app

--- embedded-app/src/main.rs
+++ embedded-app/src/main.rs
@@ -1,58 +1,69 @@
+//! # ESP32 Application code
+//! Here goes everything that is specific to the Espressif ESP32 Chip.
+//! 
+
+
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+const HEAP_SIZE: usize = 1024 * 72; 
+
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, 
-    peripherals::Peripherals, 
     prelude::*, 
-    timer::TimerGroup, 
-    embassy, 
     rmt::{Rmt, TxChannelConfig, TxChannelCreator}, 
-    gpio::IO
+    gpio::Io,
 };
+//extern crate alloc;
 
 use embassy_time::{Duration, Timer};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
 
-use static_cell::{make_static, StaticCell};
+use static_cell::StaticCell;
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+
 
 mod web;
 mod worker;
 mod client_app; //generated at compile time by build.rs
 
-#[main]
-async fn main(spawner: embassy_executor::Spawner) -> ! {
 
-
+#[esp_hal_embassy::main]
+async fn main(spawner: embassy_executor::Spawner) {
     esp_println::logger::init_logger(log::LevelFilter::Info);
+    log::info!("Initializing Hardware ;)");
 
     //ESP initializations
-    log::info!("Initializing ESP32 HAL...");
-    let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks); //-> embassy
-    let timg1 = TimerGroup::new(peripherals.TIMG1, &clocks, None); // timer0 -> esp_wifi
-    let wifi_init = esp_wifi::initialize(
+    esp_alloc::heap_allocator!(HEAP_SIZE);
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+    let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);// timer0 -> embassy
+    let timg1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);// timer0 -> esp_wifi
+    let wifi_init = esp_wifi::init(
         esp_wifi::EspWifiInitFor::Wifi,
         timg1.timer0,
         esp_hal::rng::Rng::new(peripherals.RNG),
-        system.radio_clock_control,
-        &clocks,
+        peripherals.RADIO_CLK,
     ).unwrap();
-    let (wifi_interface, wifi_controller) = esp_wifi::wifi::new_with_mode(
-        &wifi_init, 
-        peripherals.WIFI, 
-        esp_wifi::wifi::WifiStaDevice
-    ).unwrap();
-    
+    let (wifi_interface, wifi_controller) = esp_wifi::wifi::new_with_mode(&wifi_init, peripherals.WIFI, esp_wifi::wifi::WifiStaDevice).unwrap();
+
     //ESP adressable LED initializations
-    let rmt = Rmt::new(peripherals.RMT, 80.MHz(), &clocks, None).unwrap();
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let rmt = Rmt::new(peripherals.RMT, 80.MHz()).unwrap();
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let led_channel = rmt.channel0.configure(
-            io.pins.gpio13.into_push_pull_output(),
+            io.pins.gpio13,
             TxChannelConfig {
                 clk_divider: 1,
                 ..TxChannelConfig::default()
@@ -60,16 +71,21 @@ async fn main(spawner: embassy_executor::Spawner) -> ! {
     ).unwrap();
 
     //Embassy initializations
-    log::info!("Initializing embassy...");
-    embassy::init(&clocks, timg0);
+    esp_hal_embassy::init(timg0.timer0);
     let wifi_config = embassy_net::Config::dhcpv4(Default::default());
     let seed = 5489345;
-    let stack = &*make_static!(embassy_net::Stack::new(
-        wifi_interface,
-        wifi_config,
-        make_static!(embassy_net::StackResources::<5>::new()),
-        seed
-    ));
+    let stack = &*mk_static!(
+        embassy_net::Stack<esp_wifi::wifi::WifiDevice<'_, esp_wifi::wifi::WifiStaDevice>>,
+        embassy_net::Stack::new(
+            wifi_interface,
+            wifi_config,
+            mk_static!(embassy_net::StackResources<5>, embassy_net::StackResources::<5>::new()),
+            seed
+        )
+    );
+    log::info!("Embassy initialized!");
+
+
 
     //Task communication
     static LED_CTRL: StaticCell<Signal<CriticalSectionRawMutex, messages::Message>> = StaticCell::new();
@@ -89,9 +105,7 @@ async fn main(spawner: embassy_executor::Spawner) -> ! {
     spawner.spawn(web::websocket_server(stack, ctrl_signal)).ok();
     spawner.spawn(worker::worker_task(ctrl_signal, led_channel)).ok();
 
-    esp_println::println!("");//to brake up the log output
-
-    loop{Timer::after(Duration::from_millis(100)).await;}
+    loop {
+        Timer::after(Duration::from_millis(5000)).await;
+    }
 }
-
-
